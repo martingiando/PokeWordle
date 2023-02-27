3723863d326368c19e362ea16bf96d9dd497b604
martingiando <martingiando@gmail.com>
2023-02-27 15:42:27 -0300

    Clean code

--- colors.ts
+++ colors.ts
@@ -1,6 +1,5 @@
 import {
   bold,
-  white,
   bgGreen,
   bgYellow,
   bgBrightBlack,
@@ -9,10 +8,13 @@ import {
 export const colorMethods = {
   green: bgGreen,
   yellow: bgYellow,
-  gray: bgBrightBlack
+  gray: bgBrightBlack,
 }
 
-export function colorLetter (color: 'green' | 'yellow' | 'gray', letter: string) {
+export function colorLetter(
+  color: 'green' | 'yellow' | 'gray',
+  letter: string
+) {
   const bg = colorMethods[color]
   const colorizedLetter = bg(bold(` ${letter} `))
   return ` ${colorizedLetter} `
